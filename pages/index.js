aea55f1f0f5eac1ae9fb0bd2cf3f0bc27ef98658
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-02-15 00:00:35 +0530

    Styled page

--- pages/index.js
+++ pages/index.js
@@ -54,32 +54,71 @@ const WebcamStreamCapture = () => {
     });
   };
 
+  const Button = (props) => {
+    return (
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded-md m-2 shadow-lg"
+        type="button"
+        onClick={props.handler}
+      >
+        {props.children}
+      </button>
+    );
+  };
+
   return (
     <>
       <Head>
         <title>Capture Video for dataset</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Webcam audio={false} ref={webcamRef} />
-      {capturing ? (
-        <button onClick={handleStopCaptureClick}>Stop Capture</button>
-      ) : (
-        <button onClick={handleStartCaptureClick}>Start Capture</button>
-      )}
-      {recordedChunks.length > 0 && (
-        <button onClick={handleSaveVideo}>Save Video</button>
-      )}
-      <div>
-        <h3>Recorded videos:</h3>
-        {videos.map((videoURL, i) => (
-          <div key={`video_${i}`}>
-            <video style={{ width: 200 }} src={videoURL} autoPlay loop />
-            <div>
-              <button onClick={() => this.deleteVideo(videoURL)}>Delete</button>
-              <a href={videoURL}>Download</a>
-            </div>
+      <div className="container mx-auto p-0 md:p-6 lg:p-10">
+        <div className="bg-gray-100 p-6 rounded-md shadow-lg">
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            className="mx-auto my-8 w-auto h-auto lg:h-1/4 rounded-md"
+          />
+          <div className="flex justify-center">
+            {capturing ? (
+              <Button handler={handleStopCaptureClick}>Stop Capture</Button>
+            ) : (
+              <Button handler={handleStartCaptureClick}>Start Capture</Button>
+            )}
+            {recordedChunks.length > 0 && (
+              <Button handler={handleSaveVideo}>Save Video</Button>
+            )}
           </div>
-        ))}
+          <h2 className="font-bold text-3xl text-center p-4">
+            Recorded videos:
+          </h2>
+          {!videos.length && (
+            <h3 className="font-bold text-2xl text-center p-4">
+              Start recording to get dataset
+            </h3>
+          )}
+          <div className="grid grid-flow-row grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+            {videos.map((videoURL, i) => (
+              <div
+                key={`video_${i}`}
+                className="flex-col mx-auto justify-center my-2"
+              >
+                <video
+                  src={videoURL}
+                  autoPlay
+                  loop
+                  className="rounded-md mx-auto"
+                />
+                <div>
+                  <Button handler={() => deleteVideo(videoURL)}>Delete</Button>
+                  <a href={videoURL}>
+                    <Button handler={() => {}}>Download</Button>
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
