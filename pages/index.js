027487444a5106f2195abde92fdb0bda9737eda3
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-02-14 18:53:03 +0530

    Capture initialized

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,82 @@
+import React from "react";
+import Webcam from "react-webcam";
 import Head from "next/head";
 
-export default function Home() {
+const WebcamStreamCapture = () => {
+  const webcamRef = React.useRef(null);
+  const mediaRecorderRef = React.useRef(null);
+  const [capturing, setCapturing] = React.useState(false);
+  const [recordedChunks, setRecordedChunks] = React.useState([]);
+  const [videos, setVideos] = React.useState([]);
+
+  const handleStartCaptureClick = React.useCallback(() => {
+    setCapturing(true);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: "video/webm",
+    });
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef]);
+
+  const handleDataAvailable = React.useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStopCaptureClick = React.useCallback(async () => {
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+  }, [mediaRecorderRef, webcamRef, setCapturing]);
+
+  const handleSaveVideo = React.useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+      const url = URL.createObjectURL(blob);
+      setVideos((prevState) => {
+        return [...prevState, url];
+      });
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Capture Video for dataset</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </div>
+      <Webcam audio={false} ref={webcamRef} />
+      {capturing ? (
+        <button onClick={handleStopCaptureClick}>Stop Capture</button>
+      ) : (
+        <button onClick={handleStartCaptureClick}>Start Capture</button>
+      )}
+      {recordedChunks.length > 0 && (
+        <button onClick={handleSaveVideo}>Save Video</button>
+      )}
+      <div>
+        <h3>Recorded videos:</h3>
+        {videos.map((videoURL, i) => (
+          <div key={`video_${i}`}>
+            <video style={{ width: 200 }} src={videoURL} autoPlay loop />
+            <div>
+              <button onClick={() => this.deleteVideo(videoURL)}>Delete</button>
+              <a href={videoURL}>Download</a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
-}
+};
+
+export default WebcamStreamCapture;
