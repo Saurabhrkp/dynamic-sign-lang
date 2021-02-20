ee6b031933854b727620740e4ebae548ee0122bd
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-02-20 13:36:07 +0530

    Added index page

---
+++ pages/index.js
@@ -0,0 +1,81 @@
+import Head from "next/head";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Sign Language Detection</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center flex-1 text-center">
+        <h1 className="text-5xl font-bold">
+          Welcome to <a className="text-blue-600">Sign Language Detection</a>
+        </h1>
+
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          <Link href="/">
+            <a className="card">
+              <h3 className="text-2xl font-bold">
+                Static Sign Language Detection &rarr;
+              </h3>
+              <p className="mt-4 text-xl">
+                For detecting Static Signs click here!
+              </p>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="card">
+              <h3 className="text-2xl font-bold">
+                Dynamic Sign Language Detection &rarr;
+              </h3>
+              <p className="mt-4 text-xl">
+                For detecting Dynamic Signs click here!
+              </p>
+            </a>
+          </Link>
+          <Link href="/capture-photo-set">
+            <a className="card">
+              <h3 className="text-2xl font-bold">
+                Capture Static Dataset &rarr;
+              </h3>
+              <p className="mt-4 text-xl">
+                For capturing static dataset click here!
+              </p>
+            </a>
+          </Link>
+          <Link href="/capture-video-set">
+            <a className="card">
+              <h3 className="text-2xl font-bold">
+                Capture Dynamic Dataset &rarr;
+              </h3>
+              <p className="mt-4 text-xl">
+                For capturing Dynamic dataset click here!
+              </p>
+            </a>
+          </Link>
+        </div>
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <Link href="https://www.tensorflow.org/js">
+          <a
+            className="flex items-center justify-center"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by
+            <img
+              src="/tensorflow.svg"
+              alt="Tensorflowjs Logo"
+              className="h-4 ml-2"
+            />
+          </a>
+        </Link>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
