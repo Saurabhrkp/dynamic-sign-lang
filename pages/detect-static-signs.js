7297df8af9d7889c0efa3c2e6e02f99e6da318fd
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-03-03 23:35:29 +0530

    Have to fix many things

--- pages/detect-static-signs.js
+++ pages/detect-static-signs.js
@@ -5,28 +5,94 @@ import * as tf from "@tensorflow/tfjs";
 
 const StaticSign = () => {
   const webcamRef = React.useRef(null);
-  React.useEffect(async () => {
-    try {
-      const model = await tf.loadLayersModel(`/model/model.json`);
-      console.log(model);
-    } catch (error) {
-      console.error(error);
+  const [status, setStatus] = React.useState(true);
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Make Detections
+      const img = tf.browser.fromPixels(video);
+      const resized = tf.image.resizeBilinear(img, [null, 128, 128, 1]);
+      const casted = resized.cast("int32");
+      const expanded = casted.expandDims(0);
+      const obj = await net.predict(expanded);
+      console.log(obj);
+
+      const boxes = await obj[1].array();
+      const classes = await obj[2].array();
+      const scores = await obj[4].array();
+
+      console.log(boxes[0], classes[0], scores[0], 0.8);
+
+      tf.dispose(img);
+      tf.dispose(resized);
+      tf.dispose(casted);
+      tf.dispose(expanded);
+      tf.dispose(obj);
     }
-  }, []);
+  };
+
+  const stopDetection = React.useCallback(() => {
+    setStatus(false);
+  });
+
+  const startDetection = React.useCallback(async () => {
+    try {
+      setStatus(false);
+      // Load network
+      const net = await tf.loadLayersModel("/model/model.json");
+      console.log({ net });
+      // Loop and detect hands
+      console.log({ status });
+      const interval = setInterval(async () => {
+        if (status) {
+          await detect(net);
+        } else {
+          return () => clearInterval(interval);
+        }
+      }, 16000); // long interval for development
+    } catch (error) {
+      console.log(error);
+    }
+  }, [status]);
+
   return (
     <>
       <Meta title={"Static Sign Language Recognition"} />
-      <div className="container p-0 mx-auto md:p-6 lg:p-10">
-        <div className="p-6 bg-gray-100 rounded-md shadow-lg">
-          <Webcam
-            audio={false}
-            ref={webcamRef}
-            className="w-auto h-auto mx-auto my-8 rounded-md lg:h-1/4"
-          />
-        </div>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        className="w-auto h-auto mx-auto my-8 rounded-md lg:h-1/4"
+      />
+      <div className="flex-col flex-container">
+        <button
+          className="w-16 h-16 rounded-full btn btn-blue"
+          onClick={status ? startDetection : stopDetection}
+        />
+        <h1 className="font-mono text-xl font-semibold text-center text-blue-500">
+          {status ? "Start Detection" : "Stop Detection"}
+        </h1>
       </div>
     </>
   );
 };
 
 export default StaticSign;
+
+// Unhandled Runtime Error
+// Error: Error when checking : expected conv2d_input to have shape [null,128,128,1] but got array with shape [1,640,480,3].
+// Unhandled Runtime Error
+// Error: Error in resizeBilinear: new shape must 2D, but got shape ,128,128,1.
