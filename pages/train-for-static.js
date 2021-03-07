e5c6a7135e74f06c3290c2ba21825d1ef7d5315b
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-03-07 23:16:34 +0530

    Load file from zip with label

--- pages/train-for-static.js
+++ pages/train-for-static.js
@@ -4,49 +4,64 @@ import JSZip from "jszip";
 
 const TrainForStaticModel = () => {
   const [zipFiles, setZipFiles] = React.useState([]);
-  const [letter, setLetter] = React.useState(0);
-  const [photos, setPhotos] = React.useState([]);
+  const [files, setFiles] = React.useState([]);
+  const [enabled, setEnabled] = React.useState(false);
 
-  const onFileChange = (event) => {
-    setZipFiles((prevState) => {
-      return [...prevState, event.target.files[0]];
-    });
-  };
+  React.useEffect(() => {
+    if (zipFiles.length <= 2) {
+      setEnabled(false);
+    } else {
+      setEnabled(true);
+    }
+  }, [zipFiles]);
 
-  const getTrainingData = React.useCallback(
-    async (letter) => {
-      let zip = new JSZip();
-      let zipFolder = await zip.loadAsync(zipFiles[letter]);
-      zipFolder.forEach(async (_, letter) => {
-        let dataType = "data:image/jpeg;base64,";
-        let base64Data = await letter.async("base64");
-        setPhotos((prevState) => {
-          return [...prevState, `${dataType + base64Data}`];
+  const onFileChange = React.useCallback(
+    (event) => {
+      event.preventDefault();
+      if (event.target.files.length) {
+        const Files = Array.from(event.target.files);
+        Files.map((file) => {
+          return setZipFiles((prevFiles) => {
+            return [...prevFiles, file];
+          });
         });
-      });
-      setLetter((prevState) => {
-        return prevState + 1;
-      });
+      }
     },
-    [zipFiles, photos, letter]
+    [zipFiles]
   );
+
+  const getTrainingData = React.useCallback(() => {
+    zipFiles.forEach(async (zipFile, idx) => {
+      let zip = new JSZip();
+      let zipFolder = await zip.loadAsync(zipFile);
+      const image = { label: zipFile.name.split(".")[0], data: [] };
+      await zipFolder.forEach(async (_, file) => {
+        let dataType = "data:image/jpeg;base64,";
+        let base64Data = await file.async("base64");
+        image.data.push(`${dataType + base64Data}`);
+      });
+      setFiles((prevState) => {
+        return [...prevState, image];
+      });
+    });
+  }, [zipFiles, files]);
 
   return (
     <>
       <Meta title={"Train Static Model for dataset"} />
       <div className="flex-container">
-        <input type="file" onChange={onFileChange} />
-        <button onClick={() => getTrainingData(letter)}>Upload!</button>
+        <input type="file" accept=".zip" multiple onChange={onFileChange} />
+        {zipFiles.length > 2 && (
+          <button onClick={() => getTrainingData()}>Load</button>
+        )}
       </div>
-      <div className="flex-container">
-        <input type="file" onChange={onFileChange} />
-        <button onClick={() => getTrainingData(letter)}>Upload!</button>
-      </div>
-      <div className="m-8 grid-container">
-        <div className="flex-col space-y-2 flex-container">
-          <img src={photos[0]} className="rounded-md shadow-lg" />
-        </div>
-      </div>
+      {enabled ? (
+        <button type="submit">Submit</button>
+      ) : (
+        <button disabled type="submit">
+          Submit
+        </button>
+      )}
     </>
   );
 };
