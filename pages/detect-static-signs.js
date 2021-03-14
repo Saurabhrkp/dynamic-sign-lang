1042877ff3d40b6a48fbf62365aaf76608e1b6bd
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-03-14 18:07:11 +0530

    Added p5 sketch

--- pages/detect-static-signs.js
+++ pages/detect-static-signs.js
@@ -1,10 +1,57 @@
 import React from "react";
 import Webcam from "react-webcam";
+import p5 from "p5";
 import Meta from "../components/Meta";
 
 const StaticSign = () => {
   const webcamRef = React.useRef(null);
+  const P5Ref = React.useRef(null);
   const [status, setStatus] = React.useState(true);
+  const [model, setModel] = React.useState(null);
+  let ml5, P5, video, flippedVideo;
+
+  const Sketch = (p5) => {
+    p5.setup = () => {
+      createCanvas(320, 260);
+      // Create the video
+      video = createCapture(VIDEO);
+      video.size(320, 240);
+      video.hide();
+
+      flippedVideo = ml5.flipImage(video);
+      // Start classifying
+      classifyVideo();
+    };
+
+    p5.draw = () => {
+      background(0);
+      // Draw the video
+      image(flippedVideo, 0, 0);
+
+      // Draw the label
+      fill(255);
+      textSize(16);
+      textAlign(CENTER);
+      text(label, width / 2, height - 4);
+    };
+  };
+
+  React.useEffect(() => {
+    P5 = new p5(Sketch, P5Ref.current);
+  }, []);
+
+  React.useEffect(() => {
+    const IMAGE_WIDTH = 512;
+    const IMAGE_HEIGHT = 384;
+    const IMAGE_CHANNELS = 4;
+    const options = {
+      task: "imageClassification",
+      inputs: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+      outputs: ["label"],
+    };
+    ml5 = require("ml5");
+    setModel(ml5.neuralNetwork(options));
+  }, []);
 
   const detect = async () => {
     // Check data is available
